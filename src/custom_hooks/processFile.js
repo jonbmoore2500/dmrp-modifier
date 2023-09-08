96d96c992059cd1993ad951f7f2abc728e8aa947
Jonathan <jonathanmoorevla@gmail.com>
2023-09-07 21:12:49 -0500

    Fix adjusted timesheet bug in algo

--- src/custom_hooks/processFile.js
+++ src/custom_hooks/processFile.js
@@ -7,7 +7,7 @@ function processFile(data) {
         })
     }
 
-    function createProjObj(data) { // will need to break this into several smaller functions, this is going to be bonkers
+    function createProjObj(data) { // absolutely reformat and break out smaller functions, works for the time being
         
         let totaledArr = []
 
@@ -21,25 +21,33 @@ function processFile(data) {
                 })    
                 if (i1 >= 0) { // user exists in project object
                     let i2 = totaledArr[i0]["users"][i1]["timeSheets"].findIndex(element => {
-                        return element["sheetTitle"].includes(row[2].slice(0, 20)) // checks initial match of timesheet
+                        return element["sheetTitle"].includes(row[2]) // checks initial match of timesheet
                     }) 
+
+                    if (row[2].includes("Adjusted")) { // catches Adjusted timesheets, includes in that billing week
+                        i2 = totaledArr[i0]["users"][i1]["timeSheets"].findIndex(element => {
+                            return element["sheetTitle"].includes(row[2].slice(0, 20))
+                        }) 
+                    }
+
                     if (i2 >= 0 ) { // timesheet exists, update hours value
                         totaledArr[i0]["users"][i1]["timeSheets"][i2]["hours"] += row[3]
-                        if (row[2].includes("Adjusted")) {totaledArr[i0]["users"][i1]["timeSheets"][i2]["sheetTitle"] = row[2]} // adds "adjusted" to timesheet title if needed
+                        if (row[2].includes("Adjusted")) {totaledArr[i0]["users"][i1]["timeSheets"][i2]["sheetTitle"] = row[2]} // changes week to adjusted for a given user if needed
                     } else { // timesheet does not exist, create it
-                        totaledArr[i0]["users"][i1]["timeSheets"].push({"sheetTitle": row[2], "hours": row[3]})
+                        totaledArr[i0]["users"][i1]["timeSheets"] = [{"sheetTitle": row[2], "hours": row[3]}, ...totaledArr[i0]["users"][i1]["timeSheets"]]
                     }
                 } else { // user does not yet exist in project object, adds user to project object
                     totaledArr[i0]["users"].push({"userName": row[1], "timeSheets": [{"sheetTitle": row[2], "hours": row[3]}]})
                 }
-            } else { // object with prioject name does not yet exist, creates project object with first user and timesheet
+            } else { // object with project name does not yet exist, creates project object with first user and timesheet
                 totaledArr.push({"proj": row[0], "users": [{"userName": row[1], "timeSheets": [{"sheetTitle": row[2], "hours": row[3]}]}]})
             }
         })
+
         return totaledArr
     }
 
-    let finalData = createProjObj(cutExtraData(data).slice(0, 605))
+    let finalData = createProjObj(cutExtraData(data))
 
     return {"final": finalData}
 }
