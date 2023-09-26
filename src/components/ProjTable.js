5cf905521e1bb139321ee56c9b1392d7dbafd6af
Jonathan <jonathanmoorevla@gmail.com>
2023-09-25 21:07:19 -0500

    Fix idle/rates bug leaving some users out

--- src/components/ProjTable.js
+++ src/components/ProjTable.js
@@ -3,7 +3,7 @@ import popTable from "../custom_hooks/popTable"
 
 function ProjTable({project}) {
 
-    const [columnNum, setColumnNum] = useState(3)
+    const [weeksNum, setWeeksNum] = useState(3)
     const [showIdle, setShowIdle] = useState(false)
     const [showRates, setShowRates] = useState(false)
 
@@ -14,7 +14,7 @@ function ProjTable({project}) {
     let totalRow = [...fullTableObj["totals"]]
 
     if (!showIdle) {
-        tableRows = tableRows.filter((r) => r.slice(1, columnNum + 2).reduce((a, c) => a + parseInt(c), 0) > 0)
+        tableRows = tableRows.filter((r) => r.slice(2, (weeksNum + 3) * 2).some((x) => parseFloat(x) > 0))
     }
 
     if (!showRates) {
@@ -23,12 +23,8 @@ function ProjTable({project}) {
         totalRow = totalRow.filter((_, i) => i % 2 === 0)
     }
 
-    function handleRatesToggle(showBool) {
-        setShowRates(showBool)
-        setColumnNum(columnNum * (showBool ? 2 : .5))
-    }
     function handleReset() {
-        setColumnNum(3)
+        setWeeksNum(3)
         setShowIdle(false)
         setShowRates(false)
     }
@@ -43,11 +39,11 @@ function ProjTable({project}) {
                 </div>
                 <div className="buttonDiv">
                     <label>More Weeks?  </label>
-                    <button onClick={() => setColumnNum(columnNum + 3)}>More</button>
+                    <button onClick={() => setWeeksNum(weeksNum + 3)}>More</button>
                 </div>
                 <div className="buttonDiv">
                     <label>Show Rates?  </label>
-                    <button onClick={() => handleRatesToggle(!showRates)}>{!showRates ? "Show" : "Hide"}</button>
+                    <button onClick={() => setShowRates(!showRates)}>{!showRates ? "Show" : "Hide"}</button>
                 </div>
                 <div className="buttonDiv">
                     <label>Reset  </label>
@@ -59,7 +55,7 @@ function ProjTable({project}) {
                     <thead>
                         <tr>
                             <th className="firstColumn headRow">{tableHead[0]}</th>
-                            {tableHead.slice(1, columnNum + 2).map((x, i) => (
+                            {tableHead.slice(1, (weeksNum + 1) * (showRates ? 2 : 1)).map((x, i) => (
                                 <th key={x + i} className="tableHeaderCell">{(showRates && i%2 === 0 && i > 0) ? null : x}</th>
                             ))}
                         </tr>
@@ -67,18 +63,18 @@ function ProjTable({project}) {
                     <tbody>
                         {tableRows.map((r) => (
                             <tr key={r[0]}>
-                                <th className="firstColumn tableBody">{r[0]}</th> 
-                                {r.slice(1, columnNum + 2).map((x, i) => (
-                                    <td key={i} className={parseFloat(x) > 0 ? ((showRates && i%2 === 1) ? "positiveCell tableValueCell dollar" : "positiveCell tableValueCell") : "zeroCell tableValueCell"}>
+                                <td className="firstColumn tableBody">{r[0]}</td> 
+                                {r.slice(1, (weeksNum + 1) * (showRates ? 2 : 1)).map((x, i) => (
+                                    <td key={i} className={parseFloat(x) > 0 ? "positiveCell tableValueCell" : "zeroCell tableValueCell"}>
                                         {(showRates && i%2 === 0) ? "$" + x : x}
                                     </td>
                                 ))}
                             </tr>
                         ))}
-                        <tr className="totalRow">
+                        <tr>
                             <th className="firstColumn tableTotal">{totalRow[0]}</th>
-                            {totalRow.slice(1, columnNum + 2).map((x, i) => (
-                                <td key={-i} className="totalCell">{(showRates && i%2 === 0 && i > 0) ? "$" + x : x}</td>
+                            {totalRow.slice(1, (weeksNum + 1) * (showRates ? 2 : 1)).map((x, i) => (
+                                <th key={-i} className="totalCell">{(showRates && i%2 === 0 && i > 0) ? "$" + x : x}</th>
                             ))}
                         </tr>
                     </tbody>
